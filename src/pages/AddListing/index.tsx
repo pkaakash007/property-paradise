8f70631c6d57347c288c0dda9ecbda36d0bcc8cf
Perumalpuit <perumalp@uit.ac.in>
2026-08-27 19:00:01 +0530

    chore: update TODO comment text in AddListing component

--- src/pages/AddListing/index.tsx
+++ src/pages/AddListing/index.tsx
@@ -2,7 +2,7 @@ export default function AddListing() {
   return (
     <div className="min-h-screen p-8">
       <h1 className="text-2xl font-semibold text-primary">AddListing</h1>
-      {/* TODO: port design from Stitch export for this screen */}
+      {/* TODO: port design from Stitch expocvxcvxcrt for this screen */}
     </div>
   );
 }
